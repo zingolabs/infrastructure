68b5ac81445656c46a3aac9e826a215944c295f9
zancas <zancas@zingolabs.org>
2024-12-28 13:39:53 -0700

    rename script, and update test_fixtures doc-comment

--- src/test_fixtures.rs
+++ src/test_fixtures.rs
@@ -2728,12 +2728,14 @@ pub async fn get_latest_tree_state(
 /// `network` should match the network type of the cached chain.
 ///
 /// Example directory tree:
+/// ```BASH
 /// zcash-local-net/chain_cache/get_subtree_roots_sapling/
 /// └── state
 ///     └── v26
 ///         └── testnet
 ///             ├── 000008.sst
 ///             ├── 000010.sst
+/// ```
 ///
 pub async fn get_subtree_roots_sapling(
     zebrad_bin: Option<PathBuf>,
