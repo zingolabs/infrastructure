977b0d4a27567ce888a010787291e84279a9b10f
al amoda <alamoda@tutamail.com>
2025-01-06 22:13:05 -0500

    build.rs outputs textfile

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
 fn main() {
-    // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo::rerun-if-changed=test_binaries/.gitignore");
-    println!("binary changed");
+    //let out_dir = env::var_os("OUT_DIR").unwrap();
+    let out_dir = Path::new(".");
+    let dest_path = Path::new(&out_dir).join("build.rs.test.txt");
+    fs::write(&dest_path, "goodevening").unwrap();
+    //println!("cargo::rerun-if-changed=build.rs");
 }
