409f3a9ac468173b328bd6551c95b68788c2896e
al amoda <alamoda@tutamail.com>
2025-01-06 22:47:28 -0500

    build.rs test text file write to /test_binaries/

--- build.rs
+++ build.rs
@@ -1,11 +1,9 @@
-use std::env;
 use std::fs;
 use std::path::Path;
 
 fn main() {
-    //let out_dir = env::var_os("OUT_DIR").unwrap();
-    let out_dir = Path::new(".");
-    let dest_path = Path::new(&out_dir).join("build.rs.test.txt");
+    let out_dir = Path::new("./test_binaries");
+    let dest_path = Path::new(&out_dir).join("bld_outputtest.txt");
     fs::write(&dest_path, "goodevening").unwrap();
     //println!("cargo::rerun-if-changed=build.rs");
 }
