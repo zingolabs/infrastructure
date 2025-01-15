f690627bc9204ae2d480d6a77fdd8c56ac441d0c
al amoda <alamoda@tutamail.com>
2025-01-15 13:08:32 -0500

    modify clear_binaries.sh

--- utils/clear_binaries.sh
+++ utils/clear_binaries.sh
@@ -1,6 +1,6 @@
- rm ./zainod
- rm ./lightwalletd
- rm ./zcash-cli
- rm ./zcashd
- rm ./zebrad
- rm ./zingo-cli
+ rm ./fetched_resources/test_binaries/zainod/zainod
+ rm ./fetched_resources/test_binaries/lightwalletd/lightwalletd
+ rm ./fetched_resources/test_binaries/zcash-cli/zcash-cli
+ rm ./fetched_resources/test_binaries/zcashd/zcashd
+ rm ./fetched_resources/test_binaries/zebrad/zebrad
+ rm ./fetched_resources/test_binaries/zingo-cli/zingo-cli
