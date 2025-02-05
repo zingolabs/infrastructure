079b5cc3aa08be44e4402071083cfbd2e08cd9a8
nachog00 <nachogeo@zingolabs.org>
2025-02-05 02:53:37 -0300

    removed Testing binaries stuff from services/utils migrated documentation of each binary

--- fetcher/src/binaries.rs
+++ fetcher/src/binaries.rs
@@ -11,12 +11,19 @@ pub fn get_binaries_dir() -> PathBuf {
     out_dir.join("test_binaries")
 }
 
+/// Testing binaries: these are managed and fetched automagically by fetcher.rs
 pub enum SupportedBinaries {
+    /// [Lightwalletd](https://github.com/zcash/lightwalletd) is a backend service that provides a bandwidth-efficient interface to the Zcash blockchain.
     Lightwalletd,
+    /// [Zaino](https://github.com/zingolabs/zaino) is an indexer for the Zcash blockchain implemented in Rust.
     Zainod,
+    /// [Zcashd & Zcash-cli](https://zcash.readthedocs.io/en/latest/rtd_pages/zcashd.html#zcash-full-node-and-cli) allow you to run a full node and interact with it via a command-line interface. The zcashd full node downloads a copy of the Zcash blockchain, enforces rules of the Zcash network, and can execute all functionalities.
     Zcashd,
+    /// [Zcashd & Zcash-cli](https://zcash.readthedocs.io/en/latest/rtd_pages/zcashd.html#zcash-full-node-and-cli) allow you to run a full node and interact with it via a command-line interface.The zcash-cli allows interactions with the node (e.g. to tell it to send a transaction).
     ZcashCli,
+    /// [Zingo CLI](https://github.com/zingolabs/zingolib?tab=readme-ov-file#zingo-cli) is a command line lightwalletd-proxy client.
     ZingoCli,
+    /// [Zebra](https://github.com/ZcashFoundation/zebra) is a Zcash full-node written in Rust.
     Zebrad,
 }
 
