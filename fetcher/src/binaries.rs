b307cccb285a382d8aae220e306ee9a11fb2220a
nachog00 <nachogeo@zingolabs.org>
2025-02-05 02:14:29 -0300

    Implemented fetcher's public api for binaries

--- fetcher/src/binaries.rs
+++ fetcher/src/binaries.rs
@@ -11,3 +11,24 @@ pub fn get_binaries_dir() -> PathBuf {
     let out_dir = get_out_dir();
     out_dir.join("test_binaries")
 }
+
+pub enum SupportedBinaries {
+    Lightwalletd,
+    Zainod,
+    Zcashd,
+    ZcashCli,
+    ZingoCli,
+    Zebrad,
+}
+
+pub fn get_path_for_binary(binary_name: SupportedBinaries) -> PathBuf {
+    let bin_name = match binary_name {
+        SupportedBinaries::Lightwalletd => "lightwalletd",
+        SupportedBinaries::Zainod => "zainod",
+        SupportedBinaries::Zcashd => "zcashd",
+        SupportedBinaries::ZcashCli => "zcash-cli",
+        SupportedBinaries::ZingoCli => "zingo-cli",
+        SupportedBinaries::Zebrad => "zebrad",
+    };
+    get_binaries_dir().join(bin_name)
+}
