a49d5d52c346ae77b6759574e2ec7193f490e33d
nachog00 <nachogeo@zingolabs.org>
2025-02-04 22:37:40 -0300

    Deleted dummy file functions

--- fetcher/src/binaries.rs
+++ fetcher/src/binaries.rs
@@ -12,37 +12,6 @@ pub fn get_binaries_dir() -> PathBuf {
     out_dir.join("test_binaries")
 }
 
-// The following functions are only to try out the behaviour of files in OUT_DIR and accesing them from other crates
-pub mod build {
-    use std::{
-        fs::{self, File},
-        io::Write,
-        path::PathBuf,
-    };
-
-    use super::get_binaries_dir;
-    // The following functions are only to try out the behaviour of files in OUT_DIR and accesing them from other crates
-    pub fn create_test_file_with_parents() {
-        let file_path = get_test_file_path();
-
-        // Create the parent directory if it does not exist
-        if let Some(parent) = file_path.parent() {
-            fs::create_dir_all(parent).expect("Failed to create parent directories");
-        }
-
-        // Create the file
-        let mut file: File = File::create(file_path).expect("file to be created/read");
-        file.write_all(b"Dummy content")
-            .expect("file content to be written");
-    }
-
-    pub fn get_test_file_path() -> PathBuf {
-        let file_path = get_binaries_dir().join("dummy_file");
-        // Example file path
-        file_path
-    }
-}
-
 // // #![allow(dead_code)]
 // use reqwest::{Certificate, Url};
 // use sha2::{Digest, Sha512};
