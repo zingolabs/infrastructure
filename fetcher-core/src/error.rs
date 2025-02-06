8d150e809e5bfb3a8b7c3f8bf3531d36ec403b24
nachog00 <nachogeo@zingolabs.org>
2025-02-06 17:27:53 -0300

    new error.rs module

---
+++ fetcher-core/src/error.rs
@@ -0,0 +1,14 @@
+#[derive(Debug)]
+pub enum Error {
+    IoError(std::io::Error),
+    ResourceNotFound,
+    // Add other relevant error types as needed
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for Error {}
