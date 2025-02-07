89969af3759c22fa90510a020f212c846dcd57cb
nachog00 <nachogeo@zingolabs.org>
2025-02-07 03:21:46 -0300

    new get_path function in cache

--- fetcher-core/src/cache.rs
+++ fetcher-core/src/cache.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::{self, File};
 use std::io::{self, Read, Write};
 use std::path::PathBuf;
@@ -15,7 +14,7 @@ impl Cache {
     }
 
     pub fn load(&self, key: &str) -> io::Result<Vec<u8>> {
-        let path = self.store_path.join(key);
+        let path = self.get_path(key);
         let mut file = File::open(&path)?;
         let mut data = Vec::new();
         file.read_to_end(&mut data)?;
@@ -32,5 +31,13 @@ impl Cache {
     pub fn exists(&self, key: &str) -> bool {
         let path = self.store_path.join(key);
         path.exists()
+    }
+
+    pub fn get_path(&self, key: &str) -> PathBuf {
+        self.store_path.join(key)
+    }
+
+    pub fn get_store_path(&self) -> &PathBuf {
+        &self.store_path
     }
 }
