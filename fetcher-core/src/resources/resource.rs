dd757db2eb16cde2071d1c5c9edc26a6a1dd3b76
nachog00 <nachogeo@zingolabs.org>
2025-02-07 03:28:29 -0300

    implemented correct Resource trait

--- fetcher-core/src/resources/resource.rs
+++ fetcher-core/src/resources/resource.rs
@@ -1,12 +1,44 @@
-use super::super::error::Error;
+use crate::{
+    cache::Cache,
+    error::{self, Error},
+};
 
-pub trait Resource: Clone {
-    fn fetch(resource_id: &str) -> Result<Self, Error>
-    where
-        Self: Sized;
-    fn store(&self, store_path: &str) -> Result<(), Error>;
-    fn verify(&self) -> bool;
-    fn load_from_data(data: Vec<u8>) -> Result<Self, Error>
-    where
-        Self: Sized;
+pub trait Resource {
+    fn confirm(&self, cache: &Cache) -> Result<bool, Error>;
+    fn verify(&self, cache: &Cache) -> Result<bool, Error>;
+    async fn fetch(&self, cache: &Cache) -> Result<(), Error>;
+    fn get_name(&self) -> String;
+    fn get_result(&self) -> ();
+
+    async fn get(&self, cache: &Cache) -> Result<(), error::Error> {
+        // check if the resource is in cache
+        match self.confirm(&cache) {
+            Ok(res) => {
+                if !res {
+                    // if it's not, fetch it
+                    self.fetch(&cache).await;
+                } else {
+                    // not much to do here... maybe print some logs
+                }
+                // now verify that the fetched stuff is valid
+                match self.verify(cache) {
+                    Ok(is_valid) => {
+                        if is_valid {
+                            // the resource is valid
+                            println!("resource [{}] verified correctly!", self.get_name());
+                            // return the result
+                            Ok(self.get_result())
+                        } else {
+                            // the resource is invalid
+                            println!("resource [{}] invalid!", self.get_name());
+                            // throw error
+                            Err(Error::InvalidResource)
+                        }
+                    }
+                    Err(e) => todo!(),
+                }
+            }
+            Err(_e) => todo!(),
+        }
+    }
 }
