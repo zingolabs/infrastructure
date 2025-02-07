75bc4f630dc0c5ad9d8ec9fc54bc30315b4217f7
nachog00 <nachogeo@zingolabs.org>
2025-02-07 03:24:49 -0300

    removed unnecesary implementations of ResourceType

--- fetcher-core/src/resources/mod.rs
+++ fetcher-core/src/resources/mod.rs
@@ -1,30 +1,12 @@
 pub mod binaries;
 pub mod resource; // The base Resource trait // For general Binary resource types
 
+use binaries::Binaries;
+
 pub enum ResourceType {
     Binaries, // General binary category
 }
 
-impl ResourceType {
-    pub fn fetch(
-        &self,
-        resource_id: &str,
-    ) -> Result<Box<dyn resource::Resource>, crate::error::Error> {
-        match self {
-            ResourceType::Binaries => {
-                binaries::binary::BinaryResource::fetch(resource_id).map(|res| Box::new(res))
-            }
-        }
-    }
-
-    pub fn load_from_data(
-        &self,
-        data: Vec<u8>,
-    ) -> Result<Box<dyn resource::Resource>, crate::error::Error> {
-        match self {
-            ResourceType::Binaries => {
-                binaries::binary::BinaryResource::load_from_data(data).map(|res| Box::new(res))
-            }
-        }
-    }
+pub enum ResourcesEnum {
+    Binaries(Binaries),
 }
