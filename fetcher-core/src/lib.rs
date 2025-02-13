13b2f2fcdbb37118543f99b489a0cc3613545cbb
nachog00 <nachogeo@zingolabs.org>
2025-02-13 14:58:22 -0300

    moved tests away from lib.rs into tests/

--- fetcher-core/src/lib.rs
+++ fetcher-core/src/lib.rs
@@ -36,38 +36,3 @@ impl ResourcesManager {
         }
     }
 }
-
-#[tokio::test]
-async fn zainod_exists() {
-    test_utils::binary_exists(Binaries::Zainod).await
-}
-
-#[tokio::test]
-async fn lightwalletd_exists() {
-    test_utils::binary_exists(Binaries::Lightwalletd).await
-}
-
-#[tokio::test]
-async fn zcashd_exists() {
-    test_utils::binary_exists(Binaries::Zcashd).await
-}
-
-mod test_utils {
-    use crate::{Binaries, ResourcesEnum, ResourcesManager};
-
-    pub(crate) async fn binary_exists(bin: Binaries) {
-        let location = "./fetched_resources";
-        let mut manager = ResourcesManager::new(&location);
-
-        match manager.get_resource(ResourcesEnum::Binaries(bin)).await {
-            Err(e) => {
-                println!("{:}", e);
-                assert!(false)
-            }
-            Ok(bin_path) => {
-                assert!(bin_path.exists());
-                assert!(bin_path.starts_with(location));
-            }
-        };
-    }
-}
