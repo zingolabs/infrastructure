42262377c467c3715c0518582a2119576cbca680
Oscar Pepper <oscarpepper@zingolabs.org>
2025-08-22 12:05:37 +0100

    remove commented out zebrad config

--- services/src/config.rs
+++ services/src/config.rs
@@ -184,15 +184,6 @@ use_journald = false"
 [mining]
 miner_address = \"{miner_address}\"
 
-[network.testnet_parameters]
-# disable_pow = true
-# network_name = \"Regtest\"
-# network_magic = [170, 232, 63, 95]
-# slow_start_interval = 0
-# target_difficulty_limit = \"0f0f0f0000000000000000000000000000000000000000000000000000000000\"
-# genesis_hash = \"029f11d80ef9765602235e1bc9727e3eb6ba20839319f761fee920d63401e327\"
-# pre_blossom_halving_interval = 144
-
 [network.testnet_parameters.activation_heights]
 # Configured activation heights must be greater than or equal to 1,
 # block height 0 is reserved for the Genesis network upgrade in Zebra
