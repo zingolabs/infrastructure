fd919bd28e992186a8bdc05a64b23af02e3d2f9a
dorianvp <dorianvp@zingolabs.org>
2025-08-25 21:44:13 -0300

    feat: bump lrz to commit hash and add nu6_1 support

--- services/src/network.rs
+++ services/src/network.rs
@@ -44,6 +44,8 @@ pub struct ActivationHeights {
     pub nu5: BlockHeight,
     /// Nu6 network upgrade activation height
     pub nu6: BlockHeight,
+    /// Nu6_1 network upgrade activation height
+    pub nu6_1: BlockHeight,
 }
 
 impl Default for ActivationHeights {
@@ -56,6 +58,7 @@ impl Default for ActivationHeights {
             canopy: 1.into(),
             nu5: 1.into(),
             nu6: 1.into(),
+            nu6_1: 1.into(),
         }
     }
 }
@@ -71,6 +74,7 @@ impl ActivationHeights {
             NetworkUpgrade::Canopy => self.canopy,
             NetworkUpgrade::Nu5 => self.nu5,
             NetworkUpgrade::Nu6 => self.nu6,
+            NetworkUpgrade::Nu6_1 => self.nu6_1,
         }
     }
 }
