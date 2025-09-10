ba4f5962965fa450d3d0a569a48232c052e31c6f
zancas <zancas@zingolabs.org>
2025-09-10 16:37:50 -0700

    uninvert flipped logic

--- services/src/config.rs
+++ services/src/config.rs
@@ -141,7 +141,7 @@ pub(crate) fn zebrad(
     let config_file_path = config_dir.join(ZEBRAD_FILENAME);
     let mut config_file = File::create(config_file_path.clone())?;
 
-    if activation_heights.is_nu_active(
+    if !activation_heights.is_nu_active(
         zcash_protocol::consensus::NetworkUpgrade::Canopy,
         BlockHeight::from(1),
     ) {
