de9cc7a8cf0d03c88200e5bcdc43afe7d92a6d03
fluidvanadium <fluidvanadium@zingolabs.org>
2025-10-06 23:08:28 +0000

    add unit tests to executable_finder

--- zcash_local_net/src/utils/executable_finder.rs
+++ zcash_local_net/src/utils/executable_finder.rs
@@ -20,3 +20,17 @@ pub fn pick_path_from_envvar(executable_name: &str) -> Option<PathBuf> {
 // be aware these helpers are not dry because of compiler whatever
 
 pub(crate) const EXPECT_SPAWN: &str = "Failed to spawn command! Test executable must be set in TEST_BINARIES_DIR environment variable or be in PATH.";
+
+#[test]
+fn cargo() {
+    let pick_path = pick_path_from_envvar("cargo");
+    // cargo is not in the TEST_BINARIES
+    assert_eq!(pick_path, None);
+}
+#[test]
+#[ignore = "Needs TEST_BINARIES_DIR to be set and contain zcashd."]
+fn zcashd() {
+    let pick_path = pick_path_from_envvar("zcashd");
+    // cargo is not in the TEST_BINARIES
+    assert_eq!(pick_path, None);
+}
