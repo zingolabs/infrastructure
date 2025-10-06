162c694bbf9c61f1c06965af00257627a7f4a43b
fluidvanadium <fluidvanadium@zingolabs.org>
2025-10-06 23:48:17 +0000

    zig-zag

--- zcash_local_net/src/utils/executable_finder.rs
+++ zcash_local_net/src/utils/executable_finder.rs
@@ -3,18 +3,29 @@ use std::{path::PathBuf, process::Command};
 /// -Looks for an executable in TEST_BINARIES_DIR environment variable-
 /// or launches directly, hoping it is in path.
 pub fn pick_command(executable_name: &str) -> Command {
-    match pick_path_from_envvar(executable_name) {
-        Some(path) => Command::new(path),
-        None => Command::new(executable_name),
-    }
+    pick_path(executable_name)
+        .map(Command::new)
+        .unwrap_or(Command::new(executable_name))
 }
 
 /// -Checks to see if an executable is in a directory determined by the TEST_BINARIES_DIR environment variable.
-pub fn pick_path_from_envvar(executable_name: &str) -> Option<PathBuf> {
-    std::env::var("TEST_BINARIES_DIR")
-        .ok()
-        .map(|dir| PathBuf::from(dir).join(executable_name))
-        .filter(|pathbuf| pathbuf.exists())
+fn pick_path(executable_name: &str) -> Option<PathBuf> {
+    let environment_variable_path: &str = "TEST_BINARIES";
+
+    match std::env::var(environment_variable_path) {
+        Ok(directory) => {
+            let path = PathBuf::from(directory).join(executable_name);
+            if path.exists() {
+                Some(path)
+            } else {
+                None
+            }
+        }
+        Err(_err) => {
+            // tracing::warn!(format!("{environment_variable_path} environment variable is not set. It should contain the necessary executables for a test run.").as_str());
+            None
+        }
+    }
 }
 
 // be aware these helpers are not dry because of compiler whatever
@@ -23,14 +34,12 @@ pub(crate) const EXPECT_SPAWN: &str = "Failed to spawn command! Test executable 
 
 #[test]
 fn cargo() {
-    let pick_path = pick_path_from_envvar("cargo");
-    // cargo is not in the TEST_BINARIES
+    let pick_path = pick_path("cargo");
     assert_eq!(pick_path, None);
 }
 #[test]
 #[ignore = "Needs TEST_BINARIES_DIR to be set and contain zcashd."]
 fn zcashd() {
-    let pick_path = pick_path_from_envvar("zcashd");
-    // cargo is not in the TEST_BINARIES
-    assert_eq!(pick_path, None);
+    let pick_path = pick_path("zcashd");
+    assert!(pick_path.is_some());
 }
