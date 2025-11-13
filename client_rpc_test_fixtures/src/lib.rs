c6d6af67b98a612a7702808d51d8a237f3b4d1e9
Oscar Pepper <oscarpepper@zingolabs.org>
2025-11-13 17:14:19 +0000

    update zcashd chain cache and fix get_block_range_out_of_bounds test

--- client_rpc_test_fixtures/src/lib.rs
+++ client_rpc_test_fixtures/src/lib.rs
@@ -1208,11 +1208,11 @@ pub async fn get_block_range_out_of_bounds() {
     assert_eq!(zainod_blocks, lwd_blocks);
     assert_eq!(
         zainod_err_status.message(),
-        "Error: Height out of range [20]. Height requested is greater than the best chain tip [10]."
+        "Error: Height out of range [11]. Height requested is greater than the best chain tip [10]."
     );
-    assert_eq!(lwd_err_status.message(), "-8: Block height out of range");
+    assert_eq!(lwd_err_status.message(), "GetBlock: block 11 is newer than the latest block");
     assert_eq!(zainod_err_status.code(), tonic::Code::OutOfRange);
-    assert_eq!(lwd_err_status.code(), tonic::Code::Unknown);
+    assert_eq!(lwd_err_status.code(), tonic::Code::OutOfRange);
 }
 
 /// GetTransaction RPC test
