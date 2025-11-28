30f1dc3f0712d05d825b756c8cedff4a1b3bd068
Hazel OHearn <gygaxis@zingolabs.org>
2025-11-28 15:06:44 -0400

    add indexer_listen_port to Zebrad type

--- zcash_local_net/src/validator/zebrad.rs
+++ zcash_local_net/src/validator/zebrad.rs
@@ -101,10 +101,12 @@ pub struct Zebrad {
     #[getset(skip)]
     #[getset(get_copy = "pub")]
     network_listen_port: Port,
-    /// RPC listen port
+    /// json RPC listen port
     #[getset(skip)]
     #[getset(get_copy = "pub")]
     rpc_listen_port: Port,
+    /// gRPC listen port
+    indexer_listen_port: Port,
     /// Config directory
     config_dir: TempDir,
     /// Logs directory
@@ -220,6 +222,7 @@ impl Process for Zebrad {
         let zebrad = Zebrad {
             handle,
             network_listen_port,
+            indexer_listen_port,
             rpc_listen_port,
             config_dir,
             logs_dir,
